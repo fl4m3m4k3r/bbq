cf090c08a4aa30a2dee0beccd53214be2457a915
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-05-18 15:40:13 +0300

    added possablity to create event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
 
-  validates :user, presence: true
   validates :title, presence: true, length: {maximum: 255}
   validates :adress, presence: true
   validates :datetime, presence: true
