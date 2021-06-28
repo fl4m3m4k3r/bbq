febeb4c677a46fd5440fb886c877061f3eb250fb
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-06-28 13:54:33 +0300

    Subscriber cannot be event owner validation was improved and now looks similar with new validate - unauthorized user cant subscribe using email which was taken by some registrated user

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,6 +8,7 @@ class Subscription < ApplicationRecord
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
   validate :subscriber_cannot_be_event_owner
+  validate :email_is_already_taken_by_registrated_user, unless: -> { user.present? }
 
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
@@ -29,6 +30,10 @@ class Subscription < ApplicationRecord
   end
 
   def subscriber_cannot_be_event_owner
-    errors.add(:owner, "can't subscribe on own event") if event.user == user
+    errors.add(:user, :subscriber_cannot_be_event_owner) if event.user == user
+  end
+
+  def email_is_already_taken_by_registrated_user
+    errors.add(:event, :email_was_already_taken_by_some_registrated_user) if User.exists?(email: user_email)
   end
 end
