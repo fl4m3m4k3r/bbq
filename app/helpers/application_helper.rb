529e9dd0cb7b2f3f2073c4a4593dbba25fa5ec78
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-06-29 13:59:32 +0300

    Possability to add photos to events. Subscribers shows on _event partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,34 @@ module ApplicationHelper
     end
   end
 
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      image_tag(user.avatar.thumb.url)
+    else
+      image_pack_tag('media/images/user.png')
+    end
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      image_pack_tag('media/images/event.png')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      image_pack_tag('media/images/event_thumb.png')
+    end
+  end
+
   def bootstrap_class_for(flash_type)
     {
       success: 'alert-success',
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :photos
   resources :subscriptions
   devise_for :users
   devise_scope :user do
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   resources :events do
     resources :comments, only: [:create, :destroy]
     resources :subscriptions, only: [:create, :destroy]
+    resources :photos, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit]
 end
